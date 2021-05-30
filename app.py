52dddeee0247b006c922a79af2745888bdb20771
ruthple <pleschko.ruth@gmail.com>
2021-05-30 10:54:47 +0200

    initial app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+counter = 0
+
+@app.route("/")
+def hello_world():
+    global counter
+    counter = counter + 1
+    return "<p>juhu dies ist der first versuch{}</p>".format(counter)
