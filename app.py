0858e4e418824b1b2b43852af262be34a8c07e45
Nicholas Pleschko <n.pleschko@gmail.com>
2021-05-30 11:19:47 +0200

    Added bye route

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 counter = 0
 
+
 @app.route("/")
 def hello_world():
     global counter
     counter = counter + 1
     return "<p>juhu dies ist der first versuch{}</p>".format(counter)
+
+
+@app.route("/bye/123")
+def good_bye():
+    return "goodbye"
